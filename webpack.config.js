f2d2f0223ba244411bed70955932db90c0aa1705
ccjmne <ccjmne@gmail.com>
2019-05-08 19:32:21 +0200

    Set up html- and url- webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
     path: `${__dirname}/dist`,
     library: '',
     libraryTarget: 'commonjs'
+  },
+  module: {
+    rules: [{
+      test: /\.html$/,
+      use: {
+        loader: 'html-loader',
+        options: { minimize: true }
+      }
+    }, {
+      test: /\.png$/,
+      use: {
+        loader: 'url-loader'
+      }
+    }],
   }
 };
