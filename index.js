e6901daec3bafca72a9b577dbd2462846091c0a2
ccjmne <ccjmne@gmail.com>
2019-05-08 19:36:03 +0200

    Return HTML message customised w/ query params

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
-export async function handler(event, ctx) {
-  return 'Hello World';
+/**
+ * @param { mail: string, name: string, org: string, message?: string } queryStringParameters The query parameters
+ */
+export async function handler({ queryStringParameters }, ctx) {
+  return {
+    statusCode: 200,
+    body: require('./template.html').replace(/\$\{(mail|name|org|message)\}/g, (_, slot) => queryStringParameters[slot] || '<pas de message>'),
+    headers: { 'Content-Type': 'text/html' }
+  };
 }
