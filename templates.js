3f9f2ce645a8461629e9df648ca0dc64bfb994a0
ccjmne <ccjmne@gmail.com>
2019-05-10 00:02:28 +0200

    Implement templates administration utils
    Commands:
      templates list               list existing templates
      templates get <template>     get specified template
      templates create <template>  create template for specified template
      templates update <template>  update template for specified template
      templates delete <template>  delete template for specified template
    
    
    Options:
      --version  Show version number                                       
    [boolean]
      --help     Show help                                                 
    [boolean]

---
+++ templates.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const fs = require('fs');
+const yargs = require('yargs');
+const AWS = require('aws-sdk');
+const SES = new AWS.SES({ region: 'eu-west-1', credentials: new(AWS.SharedIniFileCredentials)() });
+
+const readFile = filename => new Promise((resolve, reject) => fs.readFile(filename, 'utf8', (err, buffer) => err ? reject(err) : resolve(buffer)));
+const getTemplate = yargs => yargs.positional('template', { type: 'string', describe: 'the TemplateName property identifying the SES Template' });
+
+return yargs
+  .scriptName('templates')
+  .usage('$0 <cmd> [args]')
+  .command('list', 'list existing templates', () => null, async () => {
+    const response = await SES.listTemplates().promise();
+    console.log(response);
+  })
+  .command('get <template>', 'get specified template', getTemplate, async ({ template: TemplateName }) => {
+    const response = await SES.getTemplate({ TemplateName }).promise();
+    console.log(response);
+  })
+  .command('create <template>', 'create template for specified template', getTemplate, async ({ template: TemplateName }) => {
+    const response = await SES.createTemplate({ TemplateName }).promise();
+    console.log(response);
+  })
+  .command('update <template>', 'update template for specified template', getTemplate, async ({ template: TemplateName }) => {
+    const HtmlPart = await readFile(`${__dirname}/templates/${TemplateName}.html`);
+    const response = await SES.updateTemplate({ Template: { ...require(`./templates/${TemplateName}.json`), TemplateName, HtmlPart } }).promise();
+    console.log(response);
+  })
+  .command('delete <template>', 'delete template for specified template', getTemplate, async ({ template: TemplateName }) => {
+    const response = await SES.deleteTemplate({ TemplateName }).promise();
+    console.log(response);
+  })
+  .help()
+  .argv;
